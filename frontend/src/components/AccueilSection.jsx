27fe14efa335e07c6d0eedef52bed222a348476a
Helias74 <helias.lahbib@etu.univ-lyon1.fr>
2025-05-28 13:46:35 +0200

    update home-page

--- frontend/src/components/AccueilSection.jsx
+++ frontend/src/components/AccueilSection.jsx
@@ -3,21 +3,26 @@ import './AccueilSection.css';
 function AccueilSection() {
   return (
     <section id="accueil" className="accueil-section">
-      <h1>
-        <h1>
-          <span className="ligne-haut-slogan">Doublez vos clés,</span><br />
-          <span className="ligne-bas-slogan">pas vos efforts.</span>
-        </h1>
-      </h1>
-      <p>Réservez en ligne et faites copier votre clé de voiture dans l’un de nos points de service. Sans attente inutile.</p>
+      <div className="accueil-container">
+        <div className="accueil-contenu">
+          <h1>
+            <span className="ligne-haut-slogan">Doublez vos clés,</span><br />
+            <span className="ligne-bas-slogan">pas vos efforts</span>
+            <span className="point-slogan">.</span>
+          </h1>
+          <p>
+            Réservez en ligne et faites copier votre clé de voiture dans l’un de nos points de service.
+            <span className="hilight-description"> Sans attente inutile.</span>
+          </p>
+          <a href="#reservation" className="btn-rdv">Prendre un RDV</a>
+          <a href="#services" className="en-savoir-plus">En savoir plus</a>
+        </div>
+        <div className="trapeze">
+          <h2>Contenu du trapèze</h2>
+        </div>
+      </div>
     </section>
   );
 }
 
 export default AccueilSection;
-
-
-
-
- 
-
